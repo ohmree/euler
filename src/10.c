8617cc31dfc147ebdea867c3e9b0240dd317cdcc
Omri <omrisim@gmail.com>
2017-05-02 09:33:05 +0000

    Some stuff

---
+++ src/10.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//static int sum = 0;
+
+int
+main()
+{
+/*    for (int i = 0; i <= 2000000; i++) {
+        if (i )
+    }*/
+    return EXIT_SUCCESS;
+}
---
+++ src/13.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+int
+main()
+{
+    //uint64_t meni = 37107287533902102798797998220837590246510135740250;
+    printf("%lu\n", UINT64_MAX);
+    return EXIT_SUCCESS;
+}
