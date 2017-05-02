99c022a95ec310ca98cc2dbfc783ce6c743a04ce
Omri <omrisim@gmail.com>
2017-05-02 12:39:55 +0000

    WIP

--- src/1.c
+++ src/1.c
@@ -2,30 +2,36 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define NUM 999
 
-static int
-get_multiples(int n)
-{
-    int sum = 0;
-    for (int i = 0; i <= n; i++)
-        if ((i % 3 == 0) || (i % 5 == 0))
-            sum += i;
-    return sum;
-}
+int arr[5005];
 
 int
 main(int argc, char* argv[])
 {
-    if (argc > 2) {
+    memset(arr, 0, 5005);
+/*    if (argc > 2) {
         printf("Usage: %s num\n", argv[0]);
         return EXIT_FAILURE;
     } else if (argc == 1) {
         printf("The sum of all the multiples of 3 or 5 below %d is %d\n", NUM, get_multiples(NUM));
     } else {
         printf("The sum of all the multiples of 3 or 5 below %s is %d\n", argv[1], get_multiples(atoi(argv[1])));
-    }
+    }*/
+    printf("%d\n%d\n%d\n", arr[0], arr[500], arr[5003]);
     
+    return EXIT_SUCCESS;
+}
+
+static int
+get_multiples(int n)
+{
+    int sum = 0;
+    for (int i = 0; i <= n; i++)
+        /*if ((i % 3 == 0) || (i % 5 == 0))
+            sum += i;*/
+        arr[i] = 1;
     return EXIT_SUCCESS;
 }
